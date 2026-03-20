95e746d74e626d827ae3d3e20257c8edbb1162c3
Union CF Worker <union-cf-worker@users.noreply.github.com>
2026-03-20 19:32:24 +0900

    [union-worker] set 500

--- data/status.js
+++ data/status.js
@@ -1,6 +1,6 @@
 window.SITE_STATUS = {
-  attend_num: 322,
+  attend_num: 500,
   update_time_mode: 'auto',
   update_time_text: '',
-  last_updated_iso: '2026-03-20T11:39:43+09:00'
+  last_updated_iso: '2026-03-20T19:32:24+09:00'
 };
